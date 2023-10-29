d2083f3c821e9964398fd4e51ce0d8e6ad901c8f
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-10-30 01:56:16 +0600

    added isr

--- libs/graphqlRequest.js
+++ libs/graphqlRequest.js
@@ -4,6 +4,7 @@ export default async function graphqlRequest(query) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(query),
+    next: { revalidate: 60 },
   });
 
   const json = await response.json();
